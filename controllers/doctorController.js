538bcb1de0747f601baf081665898026ebc6e150
farajallahmfatima <farajallahmfatima@gmail.com>
2020-02-26 16:53:34 +0200

    CRUD database

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -7,16 +7,19 @@ router.get('/',(req,res)=> {
     {
         style :'addstyle.css',
         viewTitle : "Create Doctor"
+
     });
-})
+});
 router.post('/',(req,res) => {
+    if(req.body._id=='')
     insertRecord(req,res);
+    else 
+    updateRecord(req,res);
 });
 function insertRecord(req,res) {
     let dr = new doctor();
     dr.name=req.body.name;
     dr.email=req.body.email;
-    dr.password=req.body.password;
     dr.mobile=req.body.mobile;
     dr.major=req.body.major;
     dr.save((err,doc)=>{
@@ -27,15 +30,57 @@ function insertRecord(req,res) {
              }
     });
 }
-router.get('/list',(req,res)=>{        
-        res.render('doctor/list.hbs', {
-            style:'liststyle.css'
-            });
+
+
+
+function updateRecord(req, res) {
+    doctor.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, doc) => {
+        if (!err) { res.redirect('doctor/list'); }
+            else {
+                console.log('Error during record update : ' + err);
+        }
+    });
+}
+
+router.get('/list',(req,res)=>{       
+        doctor.find((err,doc)=> {
+            if(!err){
+                res.render('doctor/list.hbs', {
+                    list:doc,
+                    style:'liststyle.css'
+                    });
+            }
+            else {
+                console.log('Error in retriveing doctor list : '+ err);
+            }
+        });
 });
 
 router.get('/dashbord',(req,res) => {
     res.render('doctor/dashbord.hbs',{
         style:'dashbord.css'
+    });
+});
+
+router.get('/:id', (req, res) => {
+    doctor.findById(req.params.id, (err, doc) => {
+        if (!err) {
+            res.render("doctor/addOreditDector.hbs", {
+                style:'addstyle.css',
+                viewTitle: "Update Doctor",
+                doctor: doc
+            });
+        }
+    });
+});
+
+
+router.get('/delete/:id', (req, res) => {
+    doctor.findByIdAndRemove(req.params.id, (err, doc) => {
+        if (!err) {
+            res.redirect('/doctor/list');
+        }
+        else { console.log('Error in doctor delete :' + err); }
     });
 });
 
